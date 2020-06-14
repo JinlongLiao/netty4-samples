f291f5b573005ead5e2d0d1882f85796d3258e13
liaojinlong <jinlongliao@foxmail.com>
2020-06-14 14:52:17 +0800

    # doc

--- netty-tomcat/src/main/java/org/netty/demo/tomcat/http/GPServlet.java
+++ netty-tomcat/src/main/java/org/netty/demo/tomcat/http/GPServlet.java
@@ -1,20 +1,31 @@
 package org.netty.demo.tomcat.http;
 
 public abstract class GPServlet {
-	
-	public void service(GPRequest request,GPResponse response) throws Exception{
-		
-		//由service方法来决定，是调用doGet或者调用doPost
-		if("GET".equalsIgnoreCase(request.getMethod())){
-			doGet(request, response);
-		}else{
-			doPost(request, response);
-		}
+    public static final String GET = "GET";
 
-	}
-	
-	public abstract void doGet(GPRequest request,GPResponse response) throws Exception;
-	
-	public abstract void doPost(GPRequest request,GPResponse response) throws Exception;
-	
+    public void service(GPRequest request, GPResponse response) throws Exception {
+
+        //由service方法来决定，是调用doGet或者调用doPost
+        if (GET.equalsIgnoreCase(request.getMethod())) {
+            doGet(request, response);
+        } else {
+            doPost(request, response);
+        }
+
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    public abstract void doGet(GPRequest request, GPResponse response) throws Exception;
+
+    /**
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    public abstract void doPost(GPRequest request, GPResponse response) throws Exception;
+
 }
--- netty-tomcat/src/main/java/org/netty/demo/tomcat/servlet/SecondServlet.java
+++ netty-tomcat/src/main/java/org/netty/demo/tomcat/servlet/SecondServlet.java
@@ -7,10 +7,12 @@ import org.netty.demo.tomcat.http.GPServlet;
 
 public class SecondServlet extends GPServlet {
 
-	public void doGet(GPRequest request, GPResponse response) throws Exception {
+	@Override
+    public void doGet(GPRequest request, GPResponse response) throws Exception {
 		this.doPost(request, response);
 	}
 
+	@Override
 	public void doPost(GPRequest request, GPResponse response) throws Exception {
 		response.write("This is Second Serlvet");
 	}
